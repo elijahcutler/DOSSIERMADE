0770e6fac271a686e7a5b66723b42d157e0e14a3
Elijah <elijahcutler@gmail.com>
2016-03-07 23:50:37 -0500

    Added files via upload

---
+++ javascript.js
@@ -0,0 +1,40 @@
+// JavaScript Document
+$( document ).ready(function() {
+$("#getMemed").click(function() {
+
+  $("#leftContent").fadeOut(1500, function() {
+    //Once the fadeout is complete, call fadein function
+    $("#leftLookbook").fadeIn(1500, function() {});
+  });
+
+  $("#rightContent").fadeOut(1500, function() {
+    $("#rightLookbook").fadeIn(1500, function() {});
+  });
+});
+
+$("#getLemed").click(function() {
+  $("#leftLookbook").fadeOut(1500, function() {
+    $("#leftContent").fadeIn(1500, function() {});
+  });
+
+  $("#rightLookbook").fadeOut(1500, function() {
+    $("#rightContent").fadeIn(1500, function() {});
+  });
+});
+});
+
+
+$('.showlink').each(
+    function(i){
+        $(this).text('showlink: ' + i);
+    });
+
+// Changes color on hover
+    $(function() {
+        $('.showlink').hover(function(){
+            $(this).addClass('hovered');
+        },
+        function(){
+            $(this).removeClass('hovered');
+        });
+    });
