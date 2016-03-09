bc36bdbbfc1a195139f2e85f8d536866e1d2c0e2
Elijah <elijahcutler@gmail.com>
2016-03-09 08:41:18 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,18 +1,5 @@
 // JavaScript Document
 $( document ).ready(function() {
-  
-  $("#header").click(function() {
-
-  $("#leftContent").fadeOut(1500, function() {
-    //Once the fadeout is complete, call fadein function
-    $("#leftLookbook").fadeOut(1500, function() {});
-  });
-
-  $("#rightContent").fadeOut(1500, function() {
-    $("#rightLookbook").fadeOut(1500, function() {});
-  });
-});
-  
 $("#getMemed").click(function() {
 
   $("#leftContent").fadeOut(1500, function() {
